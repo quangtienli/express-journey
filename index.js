648da0c53c3df322d09dc28d761c3f0fbce4d075
Snow31lind <tienliquang@gmail.com>
2023-05-30 01:57:50 +0700

    Add middleware

--- index.js
+++ index.js
@@ -6,12 +6,21 @@ const users = require('./users');
 
 app.use(express.static('public'));
 app.use(express.json());
+app.use((req, res, next) => {
+  console.log('Time: ', Date.now());
+  console.log('Request method: ', req.method);
+  console.log('Request URL: ', req.originalUrl);
+  next();
+});
 
 app.get('/', (req, res) => {
   res.send(`Server is running`);
 });
 
 app.use('/users', users);
+app.use((err, req, res, next) => {
+  res.status(200).send('Something broke');
+});
 
 app.listen(port, () => {
   console.log(`Server is running healthily on port ${port}`);
