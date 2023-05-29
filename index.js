ef6c02cf09c4e28f490f10864141be93e5a89bad
Snow31lind <tienliquang@gmail.com>
2023-05-30 01:31:39 +0700

    User controller

--- index.js
+++ index.js
@@ -2,9 +2,16 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+const users = require('./users');
+
+app.use(express.static('public'));
+app.use(express.json());
+
 app.get('/', (req, res) => {
   res.send(`Server is running`);
 });
+
+app.use('/users', users);
 
 app.listen(port, () => {
   console.log(`Server is running healthily on port ${port}`);
---
+++ users.js
@@ -0,0 +1,54 @@
+const express = require('express');
+const router = express.Router();
+
+const users = [
+  {
+    id: 0,
+    name: 'Li Quang Tien',
+  },
+];
+
+router.use((req, res, next) => {
+  console.log('Time: ', Date.now());
+  next();
+});
+
+router.get('/', (req, res) => {
+  res.status(200).json(users);
+});
+
+router.post('/', (req, res) => {
+  const data = req.body;
+  //   res.status(200).send(data);
+  const id = users.slice(-1)[0].id + 1;
+  const newUser = { ...data, id };
+  users.push(newUser);
+  res.status(200).json(users);
+});
+
+router.put('/:userId', (req, res) => {
+  const { userId } = req.params;
+  const existingUserIndex = users.findIndex((u) => u.id === Number(userId));
+  if (existingUserIndex < 0) {
+    return res.status(400).send(`User ${userId} not found`);
+  }
+  const data = req.body;
+  const updatedUser = {
+    ...data,
+    id: userId,
+  };
+  users.splice(existingUserIndex, 1, updatedUser);
+  res.status(200).send(updatedUser);
+});
+
+router.delete('/:userId', (req, res) => {
+  const { userId } = req.params;
+  const existingUserIndex = users.findIndex((u) => u.id === Number(userId));
+  if (existingUserIndex < 0) {
+    return res.status(400).send(`User ${userId} not found`);
+  }
+  users.splice(existingUserIndex, 1);
+  res.status(200).send(users);
+});
+
+module.exports = router;
